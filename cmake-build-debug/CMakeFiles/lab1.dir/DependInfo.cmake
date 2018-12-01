dd226189ed52d35d381e06d778e098acdf9683fc
snake0 <1260865816@qq.com>
2018-12-01 02:43:31 +0000

    initial lab4 material

---
+++ cmake-build-debug/CMakeFiles/lab1.dir/DependInfo.cmake
@@ -0,0 +1,74 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/nathan/lab-cse/lab1/test-lab-3-a.c" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/test-lab-3-a.c.o"
+  "/Users/nathan/lab-cse/lab1/test-lab-3-b.c" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/test-lab-3-b.c.o"
+  "/Users/nathan/lab-cse/lab1/test-lab2-part1-g.c" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/test-lab2-part1-g.c.o"
+  "/Users/nathan/lab-cse/lab1/test-lab2-part2-a.c" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/test-lab2-part2-a.c.o"
+  "/Users/nathan/lab-cse/lab1/test-lab2-part2-b.c" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/test-lab2-part2-b.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../lang"
+  "../proto/output"
+  "../proto/output/google"
+  "../proto/output/google/protobuf"
+  "../proto/output/google/protobuf/io"
+  "../proto/output/google/protobuf/stubs"
+  "../proto/output/google/protobuf/util"
+  "../rpc"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/nathan/lab-cse/lab1/datanode.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/datanode.cc.o"
+  "/Users/nathan/lab-cse/lab1/datanode_base.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/datanode_base.cc.o"
+  "/Users/nathan/lab-cse/lab1/demo_client.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/demo_client.cc.o"
+  "/Users/nathan/lab-cse/lab1/demo_server.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/demo_server.cc.o"
+  "/Users/nathan/lab-cse/lab1/extent_client.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/extent_client.cc.o"
+  "/Users/nathan/lab-cse/lab1/extent_server.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/extent_server.cc.o"
+  "/Users/nathan/lab-cse/lab1/extent_smain.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/extent_smain.cc.o"
+  "/Users/nathan/lab-cse/lab1/fuse.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/fuse.cc.o"
+  "/Users/nathan/lab-cse/lab1/gettime.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/gettime.cc.o"
+  "/Users/nathan/lab-cse/lab1/handle.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/handle.cc.o"
+  "/Users/nathan/lab-cse/lab1/inode_manager.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/inode_manager.cc.o"
+  "/Users/nathan/lab-cse/lab1/lock_client.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/lock_client.cc.o"
+  "/Users/nathan/lab-cse/lab1/lock_client_cache.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/lock_client_cache.cc.o"
+  "/Users/nathan/lab-cse/lab1/lock_demo.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/lock_demo.cc.o"
+  "/Users/nathan/lab-cse/lab1/lock_server.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/lock_server.cc.o"
+  "/Users/nathan/lab-cse/lab1/lock_server_cache.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/lock_server_cache.cc.o"
+  "/Users/nathan/lab-cse/lab1/lock_smain.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/lock_smain.cc.o"
+  "/Users/nathan/lab-cse/lab1/lock_tester.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/lock_tester.cc.o"
+  "/Users/nathan/lab-cse/lab1/namenode.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/namenode.cc.o"
+  "/Users/nathan/lab-cse/lab1/namenode_base.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/namenode_base.cc.o"
+  "/Users/nathan/lab-cse/lab1/proto/output/common.pb.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/proto/output/common.pb.cc.o"
+  "/Users/nathan/lab-cse/lab1/proto/output/datanode.pb.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/proto/output/datanode.pb.cc.o"
+  "/Users/nathan/lab-cse/lab1/proto/output/namenode.pb.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/proto/output/namenode.pb.cc.o"
+  "/Users/nathan/lab-cse/lab1/yfs_client.cc" "/Users/nathan/lab-cse/lab1/cmake-build-debug/CMakeFiles/lab1.dir/yfs_client.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../lang"
+  "../proto/output"
+  "../proto/output/google"
+  "../proto/output/google/protobuf"
+  "../proto/output/google/protobuf/io"
+  "../proto/output/google/protobuf/stubs"
+  "../proto/output/google/protobuf/util"
+  "../rpc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/lab1.dir/cmake_clean.cmake
@@ -0,0 +1,38 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1.dir/proto/output/common.pb.cc.o"
+  "CMakeFiles/lab1.dir/proto/output/datanode.pb.cc.o"
+  "CMakeFiles/lab1.dir/proto/output/namenode.pb.cc.o"
+  "CMakeFiles/lab1.dir/datanode.cc.o"
+  "CMakeFiles/lab1.dir/datanode_base.cc.o"
+  "CMakeFiles/lab1.dir/demo_client.cc.o"
+  "CMakeFiles/lab1.dir/demo_server.cc.o"
+  "CMakeFiles/lab1.dir/extent_client.cc.o"
+  "CMakeFiles/lab1.dir/extent_server.cc.o"
+  "CMakeFiles/lab1.dir/extent_smain.cc.o"
+  "CMakeFiles/lab1.dir/fuse.cc.o"
+  "CMakeFiles/lab1.dir/gettime.cc.o"
+  "CMakeFiles/lab1.dir/handle.cc.o"
+  "CMakeFiles/lab1.dir/inode_manager.cc.o"
+  "CMakeFiles/lab1.dir/lock_client.cc.o"
+  "CMakeFiles/lab1.dir/lock_client_cache.cc.o"
+  "CMakeFiles/lab1.dir/lock_demo.cc.o"
+  "CMakeFiles/lab1.dir/lock_server.cc.o"
+  "CMakeFiles/lab1.dir/lock_server_cache.cc.o"
+  "CMakeFiles/lab1.dir/lock_smain.cc.o"
+  "CMakeFiles/lab1.dir/lock_tester.cc.o"
+  "CMakeFiles/lab1.dir/namenode.cc.o"
+  "CMakeFiles/lab1.dir/namenode_base.cc.o"
+  "CMakeFiles/lab1.dir/test-lab-3-a.c.o"
+  "CMakeFiles/lab1.dir/test-lab-3-b.c.o"
+  "CMakeFiles/lab1.dir/test-lab2-part1-g.c.o"
+  "CMakeFiles/lab1.dir/test-lab2-part2-a.c.o"
+  "CMakeFiles/lab1.dir/test-lab2-part2-b.c.o"
+  "CMakeFiles/lab1.dir/yfs_client.cc.o"
+  "lab1.pdb"
+  "lab1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
