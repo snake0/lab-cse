2a4eef44b8a8dfaf575d63d8cfcc928616f11efd
snake0 <1260865816@qq.com>
2018-12-02 04:39:42 +0000

    finish lab3

--- lock_client_cache.cc
+++ lock_client_cache.cc
@@ -14,7 +14,7 @@ int lock_client_cache::last_port = 0;
 
 lock_client_cache::lock_client_cache(std::string xdst,
                                      class lock_release_user *_lu)
-        : lock_client(xdst), lu(_lu) {
+        : lock_client(xdst), lu(_lu), mutex() {
     srand(static_cast<unsigned int>(time(NULL) ^ last_port));
     rlock_port = ((rand() % 32000) | (0x1 << 10));
     const char *hname;
